5f76a8248daf1d8e05e594c9caabbdbeed9a1ede
hunter-tinkerer <reading.kabobs-9f@icloud.com>
2025-02-28 00:17:34 +0000

    Rearrange files

--- download_model.py
+++
@@ -1,8 +0,0 @@
-from tensorflow.keras.applications import MobileNetV2
-from tensorflow.keras.models import save_model
-
-# Load the pre-trained MobileNetV2 model
-model = MobileNetV2(weights='imagenet')
-
-# Save the model to a file
-model.save('model.h5')
